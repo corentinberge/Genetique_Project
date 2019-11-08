28a484b90d423293ddb7ae8c01cc631ee52d091a
Bergé <corentin.berge@outlook.fr>
2019-11-08 14:18:45 +0100

    add Clion project
    Add main.c and CMakefile

---
+++ cmake-build-debug/CMakeFiles/Genetique_Project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Genetique_Project.dir/main.c.o"
+  "Genetique_Project.pdb"
+  "Genetique_Project.exe"
+  "libGenetique_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Genetique_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
