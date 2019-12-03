d0153c742ef5c0719958ce22730a614391535a87
Corentin <corentin.berge@outlook.fr>
2019-12-03 15:02:40 +0100

    Problem realloc for Florian

--- main.c
+++ main.c
@@ -39,20 +39,21 @@ void lecture(FILE* f, Val* t, int* i){
 
 int main() {
     Val *tab = malloc(sizeof(Val) * SIZE);
-    if (tab == NULL){
+    if (tab == NULL) {
         printf("Erreur lors de l'allocation de l'esapce mémoire\n");
         return 1;
     }
     int size = -1;
-    FILE* fichier = NULL;
-    if((fichier = fopen("/home/corentinberge/Documents/L3_EEA/Tech_Sci/Genetique_Project/Doc/A_Genetique_Profil.txt","r")) == NULL){
+    FILE *fichier = NULL;
+    if ((fichier = fopen("/home/corentinberge/Documents/L3_EEA/Tech_Sci/Genetique_Project/Doc/A_Genetique_Profil.txt",
+                         "r")) == NULL) {
         printf("Impossible d'ouvrir le fichier !\n");
         return 2;
     }
-    lecture(fichier,tab,&size);
-    for(int j=0;j<size;j++){
+    lecture(fichier, tab, &size);
+    /*for(int j=0;j<size;j++){
         printf("%f %f %f\n",tab[j].A,tab[j].B,tab[j].C);
-    }
+    }*/
 
 
     fclose(fichier);
